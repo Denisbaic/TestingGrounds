1d766146849c3bfd7d9d7c79158f04fdffbba492
Denis <denisbaic@yandex.ru>
2019-08-10 01:56:24 +0300

    The Consequences of Inheritance

---
+++ Source/TestingGrounds/Private/PatrollingGuard.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "PatrollingGuard.h"
+
---
+++ Source/TestingGrounds/Public/PatrollingGuard.h
@@ -0,0 +1,19 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TP_ThirdPerson/TP_ThirdPersonCharacter.h"
+#include "PatrollingGuard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGGROUNDS_API APatrollingGuard : public ATP_ThirdPersonCharacter
+{
+	GENERATED_BODY()
+
+		UPROPERTY(EditInstanceOnly, Category="Patrol Routes")
+		TArray<AActor*> PatrollingPoints_CPP;
+};
